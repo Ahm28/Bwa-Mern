254582ab5a9637de5756b9ff27bf0d4a098f8664
Ahmad Mughni <ahmad.mughni1905@gmail.com>
2022-08-15 23:00:36 +0700

    :lipstick: slicing ui for header component, :sparkle: add router link

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import 'assets/scss/style.scss'
+import 'assets/scss/style.scss';
+import LandingPage from 'pages/LandingPage';
+import { Route, Routes } from 'react-router-dom'
+
 
 function App() {
   return (
     <div className="App">
-      
+      <Routes>
+        <Route path='/' element={<LandingPage />} />
+      </Routes>
     </div>
   );
 }
